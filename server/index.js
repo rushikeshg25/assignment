ee629698077dd55ebe8469eaf267cda57cbc1e6d
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2023-02-11 03:48:12 +0530

    Setting up post http request for Checkout

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const axios = require("axios");
 const app = express();
 const User = require("./models/User");
 const PORT = 3001;
+app.use(express.json());
 const URI =
   "mongodb+srv://rushikesh:rushikesh@ecommerce.alpg7vr.mongodb.net/?retryWrites=true&w=majority";
 
@@ -14,9 +15,17 @@ mongoose
   .then(() => {
     console.log("MONGO DB Connected");
   })
-  .catch((err) => console.log(err));
+  .catch((error) => console.log(error));
 
 //Checkout Logic
+app.post("/checkout", async (req, res) => {
+  try {
+    res.status(200).json(req.body.name);
+    // console.log(JSON.stringify(req.body.name));
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 //Setting up the Port
 app.listen(PORT, () => {
